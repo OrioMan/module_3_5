fb12f5b38dc412d248193a414a7e744dc7774638
OrioMan <sielsan1@gmail.com>
2024-08-21 20:00:11 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -2,7 +2,7 @@ def single_root_words(root_word, *other_word):
     same_words = []
 
     for i in other_word:
-        if root_word in i or i.lower() in root_word.lower():
+        if root_word.lower in i.lower or i.lower() in root_word.lower():
             same_words.append(i)
 
     return same_words
